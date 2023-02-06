cd6b765b68596562433ccb1dbf919f7022dfdd40
vietman2 <vietman0@gmail.com>
2023-02-06 11:56:51 +0900

    bronzev

---
+++ Python/Normal: Bronze V/#21300.py
@@ -0,0 +1,3 @@
+beer, malt, wine, soda, seltzer, water = map(int, input().split())
+
+print((beer+malt+wine+soda+seltzer+water)*5)
---
+++ Python/Normal: Bronze V/#23235.py
@@ -0,0 +1,7 @@
+i = 0
+while(True):
+    if(input() == "0"):
+        break
+    i += 1
+
+    print("Case " + str(i) + ": " + "Sorting... done!")
